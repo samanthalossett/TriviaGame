ccb0ee654730be22097f8987e21facaeeda3674f
SamanthaLossett <samanthalossett@gmail.com>
2019-05-13 18:35:10 -0700

    with more pseudo coding ideas to fix this

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,18 +21,16 @@ var data=[  // object, array, etc.//
     },
 ];
 
-$( document ).ready(function() { //"now start loading javasctipt and JQ"//
+$( document ).ready(function() {       //"now start loading javasctipt and JQ"//
         console.log( "ready!" );
 
 function appendQuestions(){
     for (var i=0; i<data.length; i++){  // for loop to iterate through my array//
            $("#questions").append("<h2>" + data[i].question + "</h2>")   
                 for (var j=0; j<data[i].answers.length; j++){
-                    $("#questions").append("<input type='radio' value=data[i].answers[j]>" + data[i].answers[j] + "</input>") // also create some value attributes// value=data[i].answers[j] inside the buttons//
-                    
-                }         
-}
-
+                    $("#questions").append("<input type='radio' value=data[i].answers[j]>" + data[i].answers[j] + "</input>") ;
+        }         
+    }
 }
 
 $(".timer").click(function() { //when button is clicked, start function starts//
@@ -49,13 +47,28 @@ function stop(){  //function to stop timer and clear the interval//
     clearInterval(interval);  
     
 }
-function keepScore(){
-    $("#questions").on("click", function(){
-        data[i].answers[j]=== correctAnswer;
-            score++;
-            console.log(score);
-        })
-}
+// function keepScore(){     // **HELP** im trying to "grab" what the user clicks and assess if === correctAnswer//
+//         // var answerValue= val(data.answers);
+
+
+//     // $("#questions").on("click", function(){
+//     //     if (data[i].answers[j]=== data.correctAnswer){
+//     //         score++;
+//     //         alert("the keepScore function is working sam");     //***HELP THIS ISNT WORKING***??// */
+//     //         console.log("is this funvtion even running?");   //no it is not//
+//     //     }
+//         // var response= data[i].answers[j];
+//         //             if(response=== data.correctAnswer){       // **HELP, tried this too, no work//
+//         //                 alert("correct!");
+//         //             }
+        
+// //         if(document.getElementById('#questions').clicked == true)    //**HELP, didnt work either// */
+// // {
+// //    alert("button was clicked");
+// // }
+
+//         })
+// }
 function reset(){   //function to start the timer all over again /
     timer=6;
     $("#questions").empty();
@@ -67,13 +80,13 @@ function decrement(){  //decrement function to decrease number from 60 by 1 and 
     if (timer=== 0){
         stop();
         alert("Times up!");
-        keepScore();
+        // keepScore();
+        alert ("You got" + " " + score + " " + "correct! - this isnt true, its just not working"); //when timer is up OR user answered each Q, end game and give result//
         $(".timer").html("Try again!");
         reset();  
     }
 }
-
 })
-// grab the user ansers, compare w/ correct answers, add tos core board  // **HELP**
-//when timer is up OR user answered each Q, end game and give result//
+// grab the user answers, compare w/ correct answers, add tos core board  // **HELP**
+
 
